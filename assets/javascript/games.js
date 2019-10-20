d66b8895f5d7ddc8c9bc617c124185390b49f351
Andre-Home <adotmorrall@gmail.com>
2019-10-19 19:52:10 -0500

    Game.js coding complete

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,16 +1,47 @@
 var matchThisNumber
-var totalScore
-var blueCrystal
-var orangeCrystal
-var purpleCrystal
-var redCrystal
+var totalScore = 0;
+var blueCrystal = $("#blue-crystal");
+var orangeCrystal = $("#orange-crystal");
+var purpleCrystal = $("#purple-crystal");
+var redCrystal = $("#red-crystal");
 var userWins = 0;
 var userLosses = 0;
 
-console.log(userWins);
-console.log(userLosses);
+var crystalValues = function () {
+    blueCrystal.attr("data-num", Math.floor(Math.random() * 15) + 1);
+    orangeCrystal.attr("data-num", Math.floor(Math.random() * 15) + 1);
+    purpleCrystal.attr("data-num", Math.floor(Math.random() * 15) + 1);
+    redCrystal.attr("data-num", Math.floor(Math.random() * 15) + 1);
+    console.log(blueCrystal.data("num"));
+    console.log(orangeCrystal.data("num"));
+    console.log(purpleCrystal.data("num"));
+    console.log(redCrystal.data("num"));
+}
 
-// Generate numbers for each jewel
+// Resets the game
+var startGame = function () {
+    matchThisNumber = Math.floor(Math.random() * 101);
+    $("#random-number").text(matchThisNumber);
+    crystalValues();
+    $("#user-total-score").text(0);
+    totalScore = 0;
+}
 
-// Blue-crystal
+startGame()
 
+$(".btn").on("click", function () {
+    totalScore = totalScore + $(this).data("num");
+    $("#user-total-score").text(totalScore);
+
+    if (totalScore === matchThisNumber) {
+        alert("Congrats, you are smarter than a 5th grader!");
+        userWins++
+        $("#win-counter").text(userWins)
+        startGame()
+    } else if (totalScore > matchThisNumber) {
+            alert("Better luck next time champ!");
+            userLosses++
+            $("#loss-counter").text(userLosses)
+            startGame()
+        }
+    })
